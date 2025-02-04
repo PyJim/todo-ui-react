d985ee6f133fd666546c996cd8fb6cc9447946aa
Jimmy Essel <jimmykodwoessel@gmail.com>
2025-02-04 16:47:36 +0000

    chore: added tests

---
+++ App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+test('dummy test: always passes', () => {
+  expect(true).toBe(true);
+});
+
+test('dummy test2: always passes', () => {
+    expect(true).toBe(true);
+  });
+
+test('dummy test3: always passes', () => {
+    expect(true).toBe(true);
+});
+
+
